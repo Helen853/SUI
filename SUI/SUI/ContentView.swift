05e2a4ee613dec9aa9b5d2208e188cad7969e9ba
@Helen853 <dudarevalen_1997@mail.ru>
2024-04-29 22:07:59 +0500

    Commit

--- SUI/SUI/ContentView.swift
+++ SUI/SUI/ContentView.swift
@@ -8,13 +8,30 @@ import SwiftUI
 struct ContentView: View {
     @State var isOnTogle = false
     var body: some View {
+        VStack(alignment: .leading) {
+            Text("Карта1").font(.system(size: 20))
+                .font(.system(size: 20, weight: .bold))
+        }
+        
+
+        Spacer(minLength: 46)
+        
         ZStack {
-            RoundedRectangle(cornerRadius: 20 ).fill(Color.red).offset(x: isOnTogle ? 100: 0)
-            Text("Text").offset(x: isOnTogle ? 100: 0)
+            RoundedRectangle(cornerRadius: 16 ).fill(Color.green).offset(x: isOnTogle ? 174: 0)
+                .frame(width: 360, height: 200)
+            Text("50 000,00 ₽").offset(x: isOnTogle ? 174: 0, y: 16)
+                .frame(width: 290, height: 30)
+                .foregroundColor(Color.white)
+            Text("доступно").offset(x: isOnTogle ? 174: 0, y: 46)
+                .frame(width: 290, height: 30)
+                .foregroundColor(Color.white)
+            
         }.animation(.spring())
+        Spacer(minLength: 458)
+        
         VStack {
             Toggle(isOn: $isOnTogle) {
-                Text("hello")
+                Text("Показать мой кошелек")
             }
         }
         .padding()
